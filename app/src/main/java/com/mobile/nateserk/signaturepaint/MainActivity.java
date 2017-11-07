38a8e55b8b39af296f1206519a265ed2f4c6bde4
Nate Kem <natek@Nates-MacBook>
2017-11-06 22:57:09 -0800

    [ Main Activity ] Cache drawing Fragment to a private variable.

--- app/src/main/java/com/mobile/nateserk/signaturepaint/MainActivity.java
+++ app/src/main/java/com/mobile/nateserk/signaturepaint/MainActivity.java
@@ -1,15 +1,19 @@
 package com.mobile.nateserk.signaturepaint;
 
+import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private DrawingFragment _drawingFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -23,15 +27,23 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
 
-                    DrawingFragment fragment = (DrawingFragment) getFragmentManager().findFragmentById(R.id.drawingBoardFragment);
-
-                    if (fragment!=null) {
-                        fragment.ResetDrawingBoard();
+                    if (_drawingFragment!=null) {
+                        _drawingFragment.ResetDrawingBoard();
                     }
 
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    if (_drawingFragment!=null) {
+                        Bitmap bitmap = _drawingFragment.GetDrawnImage();
+                        if (bitmap!=null) {
+                            Log.d("Activity DEBUG", "Bitmap Exists! Need to test actual image.");
+                            // TODO: Write to file
+                        } else {
+                            Log.d("Acitivyt DEBUG","NO BITMAP! ABORT!!!!");
+                        }
+                    }
+
                     return true;
             }
             return false;
@@ -48,4 +60,9 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
+        _drawingFragment = (DrawingFragment) getFragmentManager().findFragmentById(R.id.drawingBoardFragment);
+    }
 }
