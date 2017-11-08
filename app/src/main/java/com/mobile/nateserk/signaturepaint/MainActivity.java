366b5d197643a26246e087a9471d56f545adbc50
Nate Kem <natek@Nates-MacBook>
2017-11-07 23:34:47 -0800

    [ Save BitMap to file ] Save bitmap to a file.

--- app/src/main/java/com/mobile/nateserk/signaturepaint/MainActivity.java
+++ app/src/main/java/com/mobile/nateserk/signaturepaint/MainActivity.java
@@ -3,12 +3,17 @@ package com.mobile.nateserk.signaturepaint;
 import android.app.Fragment;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,8 +43,21 @@ public class MainActivity extends AppCompatActivity {
                         Bitmap bitmap = _drawingFragment.GetDrawnImage();
                         if (bitmap!=null) {
                             Log.d("Activity DEBUG", "Bitmap Exists! Need to test actual image.");
-                            // TODO: Write to file
+                            // TODO: Write to file and check if bitmap contains the image.
+
+                            String path = Environment.getExternalStorageDirectory().toString();
+                            File newFile = new File(path, "test.png");
+                            FileOutputStream fos = null;
+                            Log.d("Activity Debug", newFile.getPath());
+                            try {
+                                fos = new FileOutputStream(newFile);
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
                         } else {
+                            // TODO: Show Error message
                             Log.d("Acitivyt DEBUG","NO BITMAP! ABORT!!!!");
                         }
                     }
